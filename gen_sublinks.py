acc95de2393f73e66259eae3776243dd0c54815f
axwhyzee <34325457+axwhyzee@users.noreply.github.com>
2022-05-10 17:30:29 +0800

    Update gen_sublinks.py

--- gen_sublinks.py
+++ gen_sublinks.py
@@ -9,39 +9,68 @@ EDGES = []
 EDGE_LIST_DIR = 'Edgelists'
 NETWORK_GRAPH_DIR = 'Network Graphs'
 MAX_DEPTH = 2
-invalid = ['youtube.com',
-           'chrome.google',
-           'google.com',
-           'facebook.com',
-           'twitter.com',
-           'instagram.com',
-           'linkedin.com',
-           'messenger.com',
-           'bulletin.com',
-           'cloudflare.com',
-           'apps.apple',
-           'rsci.app.link',
-           'policy.medium',
-           'help.medium',
-           'medium.com/tag/',
-           'https://medium.com/@',
-           'https://medium.com/m/signin',
-           'apple.com',
-           'knowable.fyi'
-           'camerapositioning.io',
-           '/about',
-           ]
+invalid_sites = [  'youtube.com',
+                   'youtu.be',
+                   'github.com',
+                   'chrome.google',
+                   'goo.gl',
+                   'google.com',
+                   'facebook.com',
+                   'twitter.com',
+                   'instagram.com',
+                   'linkedin.com',
+                   'messenger.com',
+                   'wevolver',
+                   'reddit',
+                   'bulletin.com',
+                   'cloudflare.com',
+                   'apps.apple',
+                   'rsci.app.link',
+                   'policy.medium',
+                   'help.medium',
+                   'medium.com/tag/',
+                   'https://medium.com/@',
+                   'https://medium.com/m/signin',
+                   'apple.com',
+                   'knowable.fyi'
+                   'camerapositioning.io',
+                   '/about',
+                   '/team',
+                   '/join-us',
+                   '/contact',
+                   '/support',
+                   '/careers',
+                   '/warranty',
+                   '/shipment',
+                   '/payment-methods',
+                   '/help',
+                   '/privacy-policy'
+                   '/copyright'
+                ]
 
+invalid_ext = ['.jpg', '.jpeg', '.svg', '.png', '.js']
 
-def check_link(s):
-    if 'http' not in s:
-        return False
+def check_link(url, s):
+    if not s:
+        return ''
+
+    if url+'#'==s or url+'/#'==s:
+        return ''
     
-    for inv in invalid:
+    if 'http' not in s:
+        if (s[0] == '/' or s[0] == '#') and len(s)>1:
+            return (url + s).rstrip('/')
+        return ''
+    
+    for inv in invalid_sites:
         if inv in s:
-            return False
-    return True
+            return ''
 
+    for ext in invalid_ext:
+        if s.endswith(ext):
+            return ''
+
+    return s.rstrip('/')
 
 def get_sublinks(url):
     driver.get(url)
@@ -49,32 +78,34 @@ def get_sublinks(url):
     try:
         elems = driver.find_elements_by_tag_name('a')
         for elem in elems:
-            href = elem.get_attribute('href')
-            print(href)
-            if check_link(href) and href not in SITES:  
+            href = check_link(url, elem.get_attribute('href'))
+            if href and href not in SITES:  
                 SITES.append(href)
                 EDGES.append((url, href))
     except:
         pass
     
 
-def process_df(df):
-    df.columns = ['Blank', 'Index', 'Company Name', 'Company Description', 'Type', 'URL_1', 'URL_2']
-    df.drop(['Blank', 'Index', 'Type', 'Company Description', 'URL_1'], axis=1, inplace=True)
-    
-    return df
-
-
 driver = webdriver.Chrome(PATH)
 
 options = webdriver.ChromeOptions()
 options.add_argument('--enable-javascript')
+options.add_argument('--lang=en') 
 options.add_argument('headless')
 
-df = process_df(pd.read_excel('ComList1.xlsx'))
 
-for i, row in df.iloc[:1].iterrows():
-    url = row['URL_2']
+df = pd.read_excel('companies-sensor.xlsx')
+print(df.shape)
+df.dropna(axis=0, inplace=True)
+df['actual_url'] = df['actual_url'].apply(lambda x:x.rstrip('/'))
+print(df.shape)
+print()
+
+for i, row in df.iloc[2:].iterrows():
+    print('##############################')
+    print('Company: {}\nRoot   : {}'.format(row['Company Name'], row['actual_url']))
+    print('##############################\n')
+    url = row['actual_url']
     SITES.append(url)
 
     # iteratively add on the sublinks to our list
@@ -102,8 +133,8 @@ for i, row in df.iloc[:1].iterrows():
     net = Network('100vh', '100vw')
     net.from_nx(G)
     net.show('{}/{}.html'.format(NETWORK_GRAPH_DIR,
-                              row['Company Name']))
+                                 row['Company Name']))
 
     SITES.clear()
     EDGES.clear()
-
+    print()

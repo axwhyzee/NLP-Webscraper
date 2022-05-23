9a7e9906b6626be31d8f400bce76b541cea5de9f
axwhyzee <34325457+axwhyzee@users.noreply.github.com>
2022-05-23 22:17:35 +0800

    Update logo_detector.py

--- Lib/logo_detector.py
+++ Lib/logo_detector.py
@@ -1,7 +1,7 @@
 from img_processing import *
 from tensorflow import keras
+from genpath import gen_path
 from PIL import Image
-from genpath import *
 import numpy as np
 import requests
 import cv2
@@ -9,61 +9,41 @@ import os
 
 class LogoDetector():
     def __init__(self):
-        self.models = [keras.models.load_model(os.path.join(os.getcwd(), 'Lib', 'model_A')), # model A
-                       keras.models.load_model(os.path.join(os.getcwd(), 'Lib', 'model_B'))] # model B
-
-        self.dims = [(100, 100, 3), # i/p dimensions A
-                     (200, 200, 3)] # i/p dimensions B
-        
-        self.num_models = len(self.models)
+        self.model = keras.models.load_model(os.path.join(os.getcwd(), 'Lib', 'saved_model'))
+        self.dims = (100, 100, 3) # i/p dimensions
         
     def prepare_img(self, src):
-        paths = []
-        img_data = []
         ext = find_ext(src)
+        path = gen_path(ext)
+
+        if not download_url(src, path):
+            return [], ''
+
         try:
-            r = requests.get(src, stream=True).content
-            path = gen_path(ext)
-            paths.append(path)
-            with open(path, 'wb') as handler:
-                handler.write(r)
-        except Exception as e:
-            print('[Download Fail]', e)
-            return [], []
-
-        img = Image.open(paths[0])
-        for i in range(self.num_models):
-            dup = None
-            path = gen_path(ext)
-            
-            if self.dims[i][2] == 3:
-                dup = img.convert('RGB')
+            img = Image.open(path)
+        except:
+            print('[Error]', src)
+            return [], path
+           
+        if self.dims[2] == 3:
+            img = img.convert('RGB')
                 
-            dup = img.resize((self.dims[i][0], self.dims[i][1]))
-            dup.save(path)
-            paths.append(path)
-
-            img_data.append(cv2.imread(path))
+        img = img.resize((self.dims[0], self.dims[1]))
+        img.save(path)
+        img_data = cv2.imread(path)
         
-        return img_data, paths
+        return img_data, path
 
     def predict(self, srcs):
-        all_img_data = []
         preds = []
         for src in srcs:
-            img_data, paths = self.prepare_img(src)
-            if len(paths) == self.num_models + 1:
-                for path in paths:
-                    os.remove(path)
+            img_data, path = self.prepare_img(src)
+            if path and path in os.listdir():
+                os.remove(path)
+            if len(img_data) > 0:
+                pred = self.model.predict(np.array([img_data]))[0][0]
+                preds.append(pred)
+                
+                print('[{:.2f}] {}'.format(pred, src))
 
-                preds.append([])
-                for i in range(self.num_models):
-                    pred = self.models[i].predict(np.array([img_data[i]]))[0][0]
-                    if i==1:
-                        pred = 1-pred
-
-                    preds[-1].append(pred)
-                print('[P]', preds[-1], src)
-        print()
-            
         return preds

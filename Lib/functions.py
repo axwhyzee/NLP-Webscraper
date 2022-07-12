a290aabb9060945682d3524ea915cbd61118f6c4
axwhyzee <34325457+axwhyzee@users.noreply.github.com>
2022-07-12 20:09:53 +0800

    Add files via upload

---
+++ Lib/functions.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import pytesseract
+import requests
+import random
+import re
+import os
+
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Wei Kang\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def img_to_text(path):
+    img = Image.open(path)
+    
+    return pytesseract.image_to_string(img, lang='eng')
+
+def gen_path(ext=''):
+    path = str(random.random())[2:] + ext
+    
+    while path in os.listdir():
+        path = str(random.random())[2:] + ext
+        
+    return path
+
+def download_url(url, save_path):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36'}
+    try:
+        r = requests.get(url, verify=False, headers=headers)
+        
+        with open(save_path, 'wb') as g:
+            g.write(r.content)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def find_ext(path):
+    lower = path.lower()
+    if path:
+        for ext in ['.png', '.jpg', '.jpeg']:
+            if lower.endswith(ext):
+                return ext
+            elif ext in lower:
+                for idx in [x.end() for x in re.finditer(ext, lower)]:
+                    if idx < len(lower) and not lower[idx].isalnum():
+                        return ext
+
+    return ''

2dbfe04e2057bf2c5be5cd9a1d7587c7bacfdccd
axwhyzee <siah_weehung@yahoo.com>
2022-07-22 18:57:58 +0800

    Final

--- example.py
+++ example.py
@@ -3,7 +3,11 @@ from clientextraction.heuristic_extraction import clients_from_json, print_clien
 import pandas as pd
 import os
 
-# define variables
+'''
++------------------+
+| define variables |
++------------------+
+'''
 data_path = 'saved_data' # save scaped data here
 max_depth = 2
 client_output_path = 'clients.csv' # create client csv here
@@ -13,25 +17,7 @@ client_output_path = 'clients.csv' # create client csv here
 | Web scraping |
 +--------------+
 '''
-
-CC = CompanyCrawler(save_webtree=False, save_network_graph=True)
-
-def scrape_excel(companies):
-    # load excel data from 'companies-software.xlsx'
-    df = pd.read_excel(companies)
-    df.dropna(axis=0, inplace=True, subset=['actual_url'])
-    df.reset_index(drop=True, inplace=True)
-
-    # enumerate rows in excel
-    for i, row in df.iterrows():
-        CC.crawl_company(
-            root=row['actual_url'], 
-            company=row['Company Name'], 
-            save_dir=data_path,
-            max_depth=max_depth
-        )
-
-def scrape_single(company, url):
+def scrape_company(company, url):
     CC.crawl_company(
         root=url,
         company=company, 
@@ -44,7 +30,6 @@ def scrape_single(company, url):
 | Client extraction |
 +-------------------+
 '''
-
 def extract_clients(data_path, companies):
     results = {
         'url': [],
@@ -69,10 +54,11 @@ def extract_clients(data_path, companies):
     client_df = pd.DataFrame(results)
     client_df.to_csv(client_output_path, index=False, encoding='utf-8-sig')
 
-#scrape_single('Intermodalics', 'https://www.intermodalics.eu/')
-#scrape_single('Terabee', 'https://www.terabee.com/')
-scrape_single('Toposens', 'https://toposens.com/')
-#scrape_single('Insightness', 'https://www.insightness.com/')
-#scrape_single('Sonarax', 'https://www.sonarax.com/')
-#scrape_excel('companies-software.xlsx')
-#extract_clients(data_path, ['Intermodalics'])
+# setup webscraper
+CC = CompanyCrawler(save_webtree=False, save_network_graph=True)
+
+# scrape company
+scrape_company('ViewAR', 'https://www.viewar.com/')
+
+# convert scraped company data into client data
+extract_clients(data_path, ['ViewAR'])
